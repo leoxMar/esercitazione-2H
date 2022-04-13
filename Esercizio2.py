053ce415f53a6abe360889c05fc10891e69b95d3
leo <leonardo.marchitelli@hotmail.it>
2022-04-13 16:19:49 +0200

    piccola mod

--- Esercizio2.py
+++ Esercizio2.py
@@ -1,47 +1,47 @@
 class File_utilities:
 
            
-    def __stampa_medie_voto_e_risultati__(self):
-         nome_file=input('inserire nome file')
-         primo_esame=[]
-         secondo_esame=[]
-         nominativi=[]
-         media_voti=[]
+    def __stampa_medie_voto_e_risultati__():
+        nome_file=input('inserire nome file')
+        primo_esame=[]
+        secondo_esame=[]
+        nominativi=[]
+        media_voti=[]
          
-         try:
+        try:
           with open(f'{nome_file}','r') as saluti:
             
            lst=(saluti.readlines())
 
-         except:
-             print('file errato')
+         
 
-         for e in lst:
+          for e in lst:
                
              noNewLine = e.replace('\n','')
              primo_esame.append(noNewLine)
              secondo_esame.append(noNewLine)
              nominativi.append(noNewLine)
           
-         del nominativi [1::3]
-         del primo_esame[0::3]
-         del nominativi[1::2] 
-         del primo_esame[1::2]  
-         del secondo_esame[0::3]
-         del secondo_esame[0::2]
+          del nominativi [1::3]
+          del primo_esame[0::3]
+          del nominativi[1::2] 
+          del primo_esame[1::2]  
+          del secondo_esame[0::3]
+          del secondo_esame[0::2]
 
-         for indice in primo_esame:
+          for indice in primo_esame:
             media=(int(primo_esame[0+primo_esame.index(indice)])+int(secondo_esame[0+primo_esame.index(indice)]))/2
             media_voti.append(media)
         
-         dizionario=dict(zip(nominativi,media_voti))
+          dizionario=dict(zip(nominativi,media_voti))
        
 
-         for key,value in dizionario.items():
+          for key,value in dizionario.items():
              if value>=18:
                  print(key,'esame superato con media:' + str(value))
              else:
                  print(key,'esame non superato ')
-     
+        except:
+             print('file errato')
     
-File_utilities.__stampa_medie_voto_e_risultati__('self')
+File_utilities.__stampa_medie_voto_e_risultati__()
